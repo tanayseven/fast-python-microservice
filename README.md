e0a20f1555004c51243579cba4c60677847e6a97
Tanay PrabhuDesai <tanayseven@gmail.com>
2021-08-09 21:35:15 +0530

    Add docker-compose setup for integration tests

---
+++ README.md
@@ -0,0 +1,9 @@
+# fast-python-microservice
+
+
+## Commands
+```shell
+poetry install # to install all the packages
+./prun pytest --docker-compose=docker-compose.yaml --use-running-containers # run tests along with the docker containers
+./prun pytest test/unit
+```
--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -1,9 +1,25 @@
 from typing import Generator
 
 import pytest
+import requests
 from fastapi.testclient import TestClient
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry  # type: ignore
 
 from src.main import app
+
+
+@pytest.fixture(scope="function", autouse=True)
+def wait_for_api(function_scoped_container_getter) -> (requests.Session, str):
+    """Wait for the api from my_api_service to become responsive"""
+    request_session = requests.Session()
+    retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+    request_session.mount("http://", HTTPAdapter(max_retries=retries))
+
+    service = function_scoped_container_getter.get("adminer").network_info[0]
+    api_url = "http://%s:%s/" % (service.hostname, service.host_port)
+    assert request_session.get(api_url)
+    return request_session, api_url
 
 
 @pytest.fixture
